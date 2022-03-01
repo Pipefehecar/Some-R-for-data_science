a30ab2f78ed44fcc4fd80d684dfd980187bb24c9
Luis Herrera <pipeh.ing@gmail.com>
2022-02-28 23:49:44 -0500

    scraping finished

--- web_scraping-Angiosperms-families.R
+++ web_scraping-Angiosperms-families.R
@@ -1,31 +1,38 @@
 #install.packages("rvest")
 #install.packages("dplyr")
-
+pwd_get
 library(rvest)
 library(dplyr)
+library(stringr)
 
-link_home= "http://www.theplantlist.org/1.1/browse/"
+link_home= "http://www.theplantlist.org"
 main_page =  read_html(link_home)
 
-major_groups_scientific = html_nodes(main_page,"#nametree .majorgroup") %>% html_text()
-major_groups_clasification = html_nodes(main_page,"#nametree a") %>% html_text()
+#major_groups_scientific_name = html_nodes(main_page,"#nametree .majorgroup") %>% html_text()
+major_groups_scientific_name = html_nodes(main_page,xpath="//*[@id='nametree']/li/a/i") %>% html_text()
+major_groups_common_name = html_nodes(main_page,xpath="//*[@id='nametree']/li/a/text()") %>% html_text()
 major_groups_links = html_nodes(main_page,"#nametree a") %>% html_attr("href")
 
+
 num_major_groups <- length(major_groups_scientific)
-for ( group in major_groups_clasification) {
-  ?paste()
+
+
+plants <- data.frame()
+
+
+for(i in 1:num_major_groups){
+  # print(paste(link_home,major_groups_links[i]))
+  url = paste(link_home,major_groups_links[i]) 
+  page = read_html(str_replace(url," ",""))
+  names = html_nodes(page,".family") %>% html_text()
+  length(names)
+  for (name in names) {
+    new_row = c(name,major_groups_common_name[i],major_groups_scientific_name[i])
+    plants <- rbind(plants,new_row)
+  }
+
 }
+names(plants)<-c("NAME","GROUP_COMMON_NAME","GROUP_SCIENTIFIC_NAME")
+plants #imprimimos el dataset
 
-
-
-link = "http://www.theplantlist.org/1.1/browse/A/#B"
-page = read_html(link)
-
-
-
-
-
-
-name = page %>% html_nodes(".family") %>% html_text()
-name
-write.table(name,"Angiosperms-families.csv", quote=FALSE,sep=",",col.names	="Family names", row.names = FALSE)
+write.table(plants,"plant_list-families.csv", quote=FALSE,sep=";", row.names = FALSE)
